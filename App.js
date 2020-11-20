cae9e4c0000b5a30e32f2836185861a1a0eea97f
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-20 07:59:40 +0300

    map through the songs

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Contexts } from "./useContext"
 function App() {
+    const {songs, setSongs} = useContext(Contexts)
+    console.log(songs);
     return (
-        <p>It works</p>
+        <>
+        <h2>Hello</h2>
+        {songs.map(song => (
+                <div>
+                    <div>
+                        <h3>{song.title}</h3>
+                        <span>{song.artist}</span>
+                    </div>
+                    <button>upvote</button>
+                    <button>downvote</button>
+                </div>
+        
+        ))}
+        </>
     )
 }
 export default App;
