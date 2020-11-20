e0b3e6f2b79412e8faa4efc60bba25d569f794d6
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-20 10:50:01 +0300

    used switch and route

--- App.js
+++ App.js
@@ -1,37 +1,20 @@
 import React, { useContext } from "react"
 import { Contexts } from "./useContext"
 import Header from "./components/Header"
+import SongItem from "./components/songItem"
+import {Switch, Route} from "react-router-dom"
 function App() {
     const {songs, setSongs} = useContext(Contexts)
     console.log(songs);
     return (
-        <>
+        <div>
         <Header/>
-        {songs.map(song => (
-                <div className="song-list">
-                    <i class="ri-heart-line"></i>
-                    <div className="song">
-                        <h3>{song.title}</h3>
-                        <span>{song.artist}</span>
-                    </div>
-                    <div class="upvote">
-                        <span>{song.upvote}</span>
-                        <i class="ri-arrow-up-line"></i>
-                    </div>
-                    <div class="downvote">
-                        <span>{song.downvote}</span>
-                        <i class="ri-arrow-down-line"></i>
-                    </div>
-                    <div class="cart">
-                        <i class="ri-shopping-cart-line"></i>
-                    </div>
-                    <div class="lyrics">
-                        <i class="ri-more-line"></i>
-                    </div>
-                </div>
-        
-        ))}
-        </>
+        <Switch>
+            <Route exact path="/">
+              <SongItem/>
+            </Route>
+        </Switch>
+        </div>
     )
 }
 export default App;
