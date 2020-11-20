39cdf8457809ec6a1df1e8a103572f320ebf1531
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-20 14:24:23 +0300

    created function to update upvote and downvote

--- useContext.js
+++ useContext.js
@@ -3,11 +3,39 @@ import SongsData from "./songsData.json"
 const Contexts = React.createContext()
 function ContextProvider(props) {
     const [songs, setSongs] = useState([])
+ 
     useEffect(() => {
         setSongs(SongsData)
-    })
+    }, [])
+    function toggleUpvote(id) {
+        const upvoteUpdate = songs.map(song => {
+            if(song.id === Number(id)) {
+              console.log(song.upvote);
+              return {
+                  ...song,
+                  upvote: song.upvote + 1
+              }
+            } 
+            return {...song};
+        })
+        setSongs(upvoteUpdate)
+     }
+     function toggleDownvote(id) {
+        const downVoteUpdate = songs.map(song => {
+            if(song.id === Number(id)) {
+              console.log(song.downvote);
+              return {
+                  ...song,
+                  downvote: song.downvote + 1
+              }
+            } 
+            return {...song};
+        })
+        setSongs(downVoteUpdate)
+     }
+ 
     return (
-      <Contexts.Provider value={{songs}}>
+      <Contexts.Provider value={{songs, toggleUpvote, toggleDownvote}}>
           {props.children}
       </Contexts.Provider>
     )
