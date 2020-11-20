b71a1ac04d0e1802dbe9696c5ae0722c964c4c75
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-20 11:01:17 +0300

    imported Styles function

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Contexts } from "./useContext"
 import Header from "./components/Header"
 import SongItem from "./components/songItem"
 import {Switch, Route} from "react-router-dom"
+import Styles from "./components/styles"
 function App() {
     const {songs, setSongs} = useContext(Contexts)
     console.log(songs);
@@ -12,6 +13,9 @@ function App() {
         <Switch>
             <Route exact path="/">
               <SongItem/>
+            </Route>
+            <Route path="/styles">
+                <Styles/>
             </Route>
         </Switch>
         </div>
