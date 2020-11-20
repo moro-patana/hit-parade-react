9ab4a015c4ac1363956e830c08d4e7bb2e9390eb
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-20 11:17:19 +0300

    created form html

---
+++ components/addSong.js
@@ -0,0 +1,28 @@
+import React from "react"
+function AddSong() {
+    return (
+        <div>
+            <h3>😃Add a new song</h3>
+            <form>
+                <input type="text" placeholder="Title"/>
+                <input type="text" placeholder="Artist"/>
+                <input type="text" placeholder="Price"/>
+                <select name="pets" id="pet-select">
+                    <option value="">Styles</option>
+                    <option value="slow">Slow</option>
+                    <option value="salegy">Salegy</option>
+                    <option value="reggae">Reggae</option>
+                    <option value="kawitry">Kawitry</option>
+                    <option value="country">Country</option>
+                    <option value="rock">Rock</option>
+                    <option value="rap">Rap</option>
+                </select>
+                <label>Lyrics</label>
+                <textarea></textarea>
+                <button className="add">Add</button>
+            </form>
+        </div>
+
+    )
+}
+export default AddSong;
