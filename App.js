2b32fbe43eb33df05d93ae67ed6e8c48b09fd80c
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-21 14:05:45 +0300

    imported Lyrics function and indenting my code

--- App.js
+++ App.js
@@ -2,40 +2,43 @@ import React, { useContext } from "react"
 import { Contexts } from "./useContext"
 import Header from "./components/Header"
 import SongItem from "./components/songItem"
-import {Switch, Route} from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
 import Styles from "./components/styles"
 import AddSong from "./components/addSong"
 import Cart from "./components/cart"
 import SongStyleItem from "./components/songStyleItem"
+import Lyrics from "./components/lyrics"
 function App() {
     const { songs } = useContext(Contexts)
     const sortedSong = songs.sort((songA, songB) => {
         const ratioA = songA.upvote - songA.downvote;
         const ratioB = songB.upvote - songB.downvote;
         return ratioB - ratioA;
-  });
+    });
     return (
         <div>
-        <Header/>
-        <Switch>
-            <Route exact path="/">
-            {sortedSong.map(song => ( <SongItem key={song.id} song={song}/> ))}
-            </Route>
-            <Route exact path="/styles">
-                <Styles/>
-            </Route>
-            <Route path="/styles/:cartStyle">
-                <SongStyleItem/>
-            </Route>
-            <Route path="/addsong">
-            <h4>😃Add a new song</h4>
-                <AddSong/>
-            </Route>
-            <Route exact path="/cart">
-                <Cart/>
-            </Route>
-            <Route path="/cart/:cartId"></Route>
-        </Switch>
+            <Header />
+            <Switch>
+                <Route exact path="/">
+                    {sortedSong.map(song => (<SongItem key={song.id} song={song} />))}
+                </Route>
+                <Route exact path="/styles">
+                    <Styles />
+                </Route>
+                <Route path="/styles/:cartStyle">
+                    <SongStyleItem />
+                </Route>
+                <Route path="/addsong">
+                    <h4>😃Add a new song</h4>
+                    <AddSong />
+                </Route>
+                <Route exact path="/cart">
+                    <Cart />
+                </Route>
+                <Route path="/lyrics/:songId">
+                    <Lyrics />
+                </Route>
+            </Switch>
         </div>
     )
 }
