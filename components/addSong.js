b792fe2dbee90d495a5bc4b01537c9f1b52d9d5b
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-21 14:08:22 +0300

    created new song list and push it into the songs

--- components/addSong.js
+++ components/addSong.js
@@ -1,12 +1,32 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Contexts } from "../useContext";
 function AddSong() {
+    const { songs, setSongs } = useContext(Contexts)
+    function handleSubmit(e) {
+        e.preventDefault();
+        const input = e.target
+        const newSong = {
+            title: input.title.value,
+            artist: input.artist.value,
+            price: input.price.value,
+            style: input.style.value,
+            lyrics: input.lyrics.value,
+            isfavorite: "false",
+            upvote: 0,
+            downvote: 0,
+            id: Date.now()
+        }
+        songs.push(newSong)
+        setSongs([...songs])
+        e.target.reset()
+    }
     return (
         <div className="add-new-song">
-            <form>
-                <input type="text" name="title" placeholder="Title"/>
-                <input type="text" name="artist" placeholder="Artist"/>
-                <input type="text" name="price" placeholder="Price"/>
-                <select name="pets" id="pet-select">
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="title" placeholder="Title" />
+                <input type="text" name="artist" placeholder="Artist" />
+                <input type="text" name="price" placeholder="Price" />
+                <select name="style" id="pet-select">
                     <option value="">Styles</option>
                     <option value="slow">Slow</option>
                     <option value="salegy">Salegy</option>
@@ -17,8 +37,8 @@ function AddSong() {
                     <option value="rap">Rap</option>
                 </select>
                 <label>Lyrics</label>
-                <textarea></textarea>
-                <button className="add">Add</button>
+                <textarea name="lyrics"></textarea>
+                <button type="submit" className="add">Add</button>
             </form>
         </div>
 
