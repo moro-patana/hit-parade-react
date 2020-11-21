9d690f65c41c2ae5e4fefc86fac42e948a024ae6
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-21 08:08:52 +0300

    map through the cartItem

--- components/cart.js
+++ components/cart.js
@@ -1,19 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Contexts } from "../useContext";
 function Cart() {
+    const { cartItem } = useContext(Contexts)
+    const mapCartItem = cartItem.map(item => (<h1>{item.title}</h1>))
     return (
+        <>
+        {cartItem.map(item => (
         <div>
-            <div class="song-list">
-                <i class="ri-delete-bin-line"></i>
-                <div className="song">
-                  <h3>Title</h3>
-                  <span>Artist</span>
-                </div>
-                <p className="price">0 Ar</p>
+        <div class="song-list">
+            <i class="ri-delete-bin-line"></i>
+            <div className="song">
+              <h3>{item.title}</h3>
+              <span>{item.artist}</span>
             </div>
-            <p className="totalPrice">Total:</p>
-            <button className="buy">Buy</button>
+            <p className="price">{item.price}</p>
         </div>
+        <p className="totalPrice">Total:</p>
+        <button className="buy">Buy</button>
+    </div>
 
+        ))}
+</>
     )
 }
 export default Cart;
